cec6f0aaf15d746ec80b89e3d72046ee30fe6085
Markus Storm <markus.storm@gmx.net>
2019-07-22 23:31:28 +0200

    minor restructuring part 2
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/zram.bash
+++ functions/zram.bash
@@ -16,38 +16,4 @@ init_zram_mounts() {
   fi
 }
 
-Xcheck_zram_mounts() {
-  local FILE=/etc/ztab
-  local i=0
-
-  if ! [ -f ${FILE} ]; then
-    echo "ZRAM not installed - test successful."
-    return 0
-  fi
-
-  while read -r line; do
-    case "${line}" in
-      "#"*) continue ;;
-      "")   continue ;;
-      *)    set -- $\{line\}
-            TYPE=$1
-            if [ "${TYPE}" == "swap" ]; then
-	      if $\(/sbin/swapon | /bin/grep -q zram\); then
-                echo "$(basename "$0") error: swap not on zram"
-                return 1
-              fi
-            else
-              if [ "$(df $5 | awk '/overlay/ { print $1 }')" != "overlay${i}" ]; then
-                echo "$(basename "$0") error: overlay${i} not found"
-                return 1
-              fi
-            fi
-            let i=$i+1
-            ;;
-    esac
-  done < "$FILE"
-  echo "ZRAM successfully installed."
-
-  return 0
-}
 
