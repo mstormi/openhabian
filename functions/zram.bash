e63d385a95f2f35a9ae9f006ed4e811860bce1c8
Markus Storm <markus.storm@gmx.net>
2019-07-23 11:30:57 +0200

    use validated branch sysv-init of zram code instead of master
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/zram.bash
+++ functions/zram.bash
@@ -1,9 +1,10 @@
 init_zram_mounts() {
   if [ "$1" == "install" ]; then
     local ZRAMGIT=https://github.com/StuartIanNaylor/zram-config
+    local BRANCH=sysv-init
     TMP="$(mktemp -d /tmp/.XXXXXXXXXX)"
 
-    /usr/bin/git clone ${ZRAMGIT} ${TMP}
+    /usr/bin/git clone -b ${BRANCH} ${ZRAMGIT} ${TMP}
     cd ${TMP}
     /bin/sh ./install.sh
     /usr/bin/install -m 644 ${BASEDIR}/includes/ztab /etc/ztab
